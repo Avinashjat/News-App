bb3d66fcf340982e7f394a89d5cf8ee6bae2a16c
Avinash Jat <dev.avinashjat@gmail.com>
2023-09-20 02:03:55 +0530

    fetch news api in the news world

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,66 +1,101 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+// import React, { Component } from 'react'
+// import NewsItem from './NewsItem'
+
+// export class News extends Component {
+
+    
+//     constructor(){
+//       console.log("i am consuturor")
+//         super();
+//        this.state ={
+//                articals :[ ],
+//                loading : false
+//        }
+        
+//     }
+
+//    async componentDidMount(){
+//      let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=94903894357049f3adb1f24ae0b50794"
+//      let data = await fetch(url);
+//      let parsedData = await data.json();
+//      this.setState({articals :parsedData.articals })
+//     }
+
+//   render() {
+ 
+//     return (
+//       <div className = " container my-3">
+//         <h2> News World - Top headline </h2>
+      
+   
+//              <div className="row">
+//              {this.state.articals.map((element)=>{
+//               return <div className="col-md-4" key={element.url}>
+//               <NewsItem  title={element.title?element.title.slice(0,44):""} discription={element.description?element.description.slice(0,88):""}  imgurl ={element.urlToImage} newsurl={element.url} />
+//               </div>
+
+//               })}
+                
+//            </div>
+           
+//       </div>
+//     )
+//   }
+// }
+
+// export default News
+
+
+
+
+
+
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 
 export class News extends Component {
-    artical = [
+  constructor() {
+    super();
+    this.state = {
+      articles: [], 
+      loading: true,
+    };
+  }
 
-        {"source":{"id":"al-jazeera-english",
-    "name":"Al Jazeera English"},"author":"Hafsa Adil",
-    "title":"‘Every day is a struggle’: Sri Lanka banks on Asia Cup cricket tournament",
-"description":"Cricket is bringing temporary relief to a struggling Sri Lankan economy reeling from post-COVID shortages and inflation.",
-"url":"http://www.aljazeera.com/sports/2023/9/16/sri-lanka-banks-on-asia-cup-cricket-tournament",
-"urlToImage":"https://www.aljazeera.com/wp-content/uploads/2023/09/33V82NE-highres-1694809059.jpg?resize=1920%2C1440",
-"publishedAt":"2023-09-16T09:42:46Z",
-"content":"Colombo, Sri Lanka Until a few months ago, SMS Senaratne struggled to put three meals on the table for his family.\r\nWhen the economic crisis struck the country two years ago it took away our savings,… [+4157 chars]"},
-
-{"source":{"id":"espn-cric-info",
-"name":"ESPN Cric Info"},"author":null,
-"title":"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-"description":"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-"url":"http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-"urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg","publishedAt":"2020-04-27T11:41:47Z",
-"content":"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"},
-
-{"source":{"id":"espn-cric-info",
-"name":"ESPN Cric Info"},"author":null,
-"title":"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-"description":"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-"url":"http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-"urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-"publishedAt":"2020-03-30T15:26:05Z","content":"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-    ]
-    
-    constructor(){
-        super();
-       this.state ={
-               articals : this.artical,
-               loading : false
-       }
-        
-    }
+  async componentDidMount() {
+    let url =
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=94903894357049f3adb1f24ae0b50794';
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles }); 
+  }
 
   render() {
     return (
-      <div className = " container my-3">
-        <h2> News World - Top headline </h2>
-             <div className="row">
-                <div className="col-md-4">
-           <NewsItem title="News-Box"  discription="its is a new app "  imgurl ="https://www.aljazeera.com/wp-content/uploads/2023/09/33V82NE-highres-1694809059.jpg?resize=1920%2C1440" />
-           </div>
+      <div className="container my-3">
+        <h2>News World - Top headlines</h2>
 
-           <div className="col-md-4">
-           <NewsItem title="News-Box"  discription="its is a new app " />
-           </div>
-
-           <div className="col-md-4">
-           <NewsItem title="News-Box"  discription="its is a new app " />
-           </div>
-
-           </div>
+        <div className="row">
+          {this.state.articles.length > 0 ? ( // Check if articles array is not empty
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 44) : ''}
+                    description={element.description ? element.description.slice(0, 88) : ''}
+                    imgurl={element.urlToImage}
+                    newsurl={element.url}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title , discription,imgurl}= this.props
+    let {title , discription,imgurl,newsurl}= this.props
     return (
       <div className="my-3">
            <div className="card" style={{width: "18rem"}}>
-  <img src={imgurl} className="card-img-top" alt="..." />
+  <img src={imgurl?imgurl:"https://english.cdn.zeenews.com/sites/default/files/2023/09/18/1283694-aditya-l1-isro.jpg"} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{discription}</p>
-    <a href="/" className="btn btn-sm btn-primary">Read More</a>
+    <a href={newsurl} target='_blank)' className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
       </div>
